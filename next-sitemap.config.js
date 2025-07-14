c017ce8260781e9939566d7e8d2fc09de0079a2a
mahmoudAliaboElhassan <ml6893254@gmail.com>
2025-07-14 07:48:39 +0300

    adding some features

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,19 +2,30 @@
 module.exports = {
   siteUrl: "https://posts-crud-fullstack.vercel.app",
   generateRobotsTxt: true,
-  generateIndexSitemap: true,
+  generateIndexSitemap: false, // Try setting this to false first
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    };
+  },
   additionalPaths: async (config) => [
     {
       loc: "/",
       changefreq: "daily",
       priority: 1.0,
+      lastmod: new Date().toISOString(),
     },
     {
       loc: "/posts",
       changefreq: "daily",
       priority: 0.9,
+      lastmod: new Date().toISOString(),
     },
-
-    // لو عندك صفحات تانية زي /auth/login أو /dashboard زودها هنا
   ],
 };
