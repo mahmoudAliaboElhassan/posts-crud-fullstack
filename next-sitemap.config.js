af9b4c9d308e7b0d3f75c07bbea6e178417db66c
mahmoudAliaboElhassan <ml6893254@gmail.com>
2025-07-14 06:58:20 +0300

    adding some routes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://posts-crud-fullstack.vercel.app/", // غيّرها لدومينك
+  siteUrl: "https://posts-crud-fullstack.vercel.app",
   generateRobotsTxt: true,
   generateIndexSitemap: true,
+  additionalPaths: async (config) => [
+    {
+      loc: "/",
+      changefreq: "daily",
+      priority: 1.0,
+    },
+    {
+      loc: "/posts",
+      changefreq: "daily",
+      priority: 0.9,
+    },
+    {
+      loc: "/comments",
+      changefreq: "weekly",
+      priority: 0.7,
+    },
+    // لو عندك صفحات تانية زي /auth/login أو /dashboard زودها هنا
+  ],
 };
