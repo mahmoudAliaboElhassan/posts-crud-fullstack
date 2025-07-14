dc69d12127b1ec742df4253f4bc93969177d9709
mahmoudAliaboElhassan <ml6893254@gmail.com>
2025-07-14 06:49:47 +0300

    adding sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://posts-crud-fullstack.vercel.app/", // غيّرها لدومينك
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+};
